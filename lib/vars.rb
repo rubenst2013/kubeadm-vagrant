37fbfa71eef450f92269344bb8e9bd820095b3fb
Ruben Steinbacher <rsteinbacher@gk-software.com>
2019-12-21 07:16:29 +0100

    Use latest docker-ce 19.03.5

--- lib/vars.rb
+++ lib/vars.rb
@@ -5,8 +5,8 @@ $MASTER_IP    = "192.168.26.10"
 $MASTER_PORT  = "8443"
 $NODE_IP_NW   = "192.168.26."
 $POD_NW_CIDR  = "10.244.0.0/16"
-
-$DOCKER_VER = "5:19.03.3~3-0~ubuntu-disco"
+               
+$DOCKER_VER = "5:19.03.5~3-0~ubuntu-bionic"
 $KUBE_VER   = "1.17.0"
 $KUBE_TOKEN = "ayngk7.m1555duk5x2i3ctt"
 $IMAGE_REPO = "registry.aliyuncs.com/google_containers"
