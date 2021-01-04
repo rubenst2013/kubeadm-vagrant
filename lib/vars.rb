e8e8c4b6709987e4618b52ab37440eca93505397
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2021-01-04 07:34:30 +0100

    Update vars
    - update Docker to 19.03.13
    - update Kubernetes to 1.19.6

--- lib/vars.rb
+++ lib/vars.rb
@@ -1,13 +1,13 @@
 $BOX_IMAGE    = "ubuntu/disco64"
-$MASTER_COUNT = 1
-$NODE_COUNT   = 2
+$MASTER_COUNT = 3
+$NODE_COUNT   = 3
 $MASTER_IP    = "192.168.26.10"
 $MASTER_PORT  = "8443"
 $NODE_IP_NW   = "192.168.26."
 $POD_NW_CIDR  = "10.244.0.0/16"
                
-$DOCKER_VER = "5:19.03.12~3-0~ubuntu-focal"
-$KUBE_VER   = "1.18.5"
+$DOCKER_VER = "5:19.03.13~3-0~ubuntu-focal"
+$KUBE_VER   = "1.19.6"
 $KUBE_TOKEN = "ayngk7.m1555duk5x2i3ctt"
 $IMAGE_REPO = "k8s.gcr.io" # "registry.aliyuncs.com/google_containers"
 
